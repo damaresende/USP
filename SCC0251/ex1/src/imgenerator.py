2bfd24a9bda4dfc50aaaedfe12661861e05df6c9
damaresresende <dc.oliveiraresende@gmail.com>
2019-03-31 04:37:29 -0300

    RMSE validation

--- SCC0251/ex1/src/imgenerator.py
+++ SCC0251/ex1/src/imgenerator.py
@@ -12,7 +12,7 @@ Image generator
 
 import random
 import numpy as np
-from math import sin, cos, pow, floor
+from math import sin, cos, pow, floor, sqrt
 
 
 class IMGenerator():
@@ -57,12 +57,12 @@ class IMGenerator():
         Function 1: generates images from f(x,y) = (xy + 2x)
         '''
         img = np.ones((self.C, self.C), dtype='float32')
-        
+
         for x in range(self.C):
             for y in range(self.C):
                 img[x][y] = x * y + 2 * y
         
-        return img
+        return self.quantitize(self.downsampling(self.normalize(img, 16)))
     
     def func_two(self):
         '''
@@ -74,7 +74,7 @@ class IMGenerator():
             for y in range(self.C):
                 img[x][y] = abs(cos(x / self.Q) + 2 * sin(y / self.Q))
                                 
-        return img
+        return self.quantitize(self.downsampling(self.normalize(img, 16)))
     
     def func_three(self):
         '''
@@ -86,36 +86,39 @@ class IMGenerator():
             for y in range(self.C):
                 img[x][y] = abs(3 * (x / self.Q) - pow(y/self.Q, 1/3))
                                 
-        return img
+        return self.quantitize(self.downsampling(self.normalize(img, 16)))
     
     def func_four(self):
         '''
         Function 4: generates images from f(x,y) = random(0,1,S)
         '''
-        random.seed(self.S)
-        img = np.ones((self.C, self.C), dtype='float32')
+        random.seed(self.B)
+        img = np.zeros((self.C, self.C), dtype='float32')
         
         for x in range(self.C):
             for y in range(self.C):
-                img[x][y] = random.randint(0, 1)
-                                
-        return img
+                img[x][y] = random.random()
+        
+        return self.quantitize(self.downsampling(self.normalize(img, 16)))
     
     def func_five(self):
         '''
         Function 5: generates images from f(x,y) = randomwalk(S)
         '''
-        random.seed(self.S)
+        random.seed(self.B)
         img = np.zeros((self.C, self.C), dtype='float32')
         
         x = 0
         y = 0
-        for _ in range(0, 1 + self.C * self.C):
-                x = (x + random.randint(-1,1)) % self.C
-                y = (y + random.randint(-1,1)) % self.C
-                img[x][y] = 1
-                                
-        return img
+        
+        for _ in range(floor((self.C * self.C + 1) / 2)):
+            img[x][y] = 1
+            x = (x + random.randint(-1,1)) % self.C
+            y = (y + random.randint(-1,1)) % self.C
+            
+            
+
+        return self.quantitize(self.downsampling(self.normalize(img, 16)))
     
     def normalize(self, img, B):
         '''
@@ -137,8 +140,38 @@ class IMGenerator():
         '''
         new_img = np.zeros((self.N, self.N))
         
+#         for x in range(0, self.N):
+#             for y in range(0, self.N):
+#                 new_img[x][y] = img[floor(x * self.C / self.N)][floor(y * self.C / self.N)]
+
+        r = floor(self.C / self.N)
         for x in range(0, self.N):
             for y in range(0, self.N):
-                new_img[x][y] = img[floor(x * self.C / self.N)][floor(y * self.C / self.N)]
+                new_img[x][y] = img[x * r][y * r]
             
         return new_img
+
+    def quantitize(self, img):
+        '''
+        Reduces the number of bits that represents each pixel in the image
+        
+        @param img: 2D numpy array to reduce dimensions
+        '''
+        img = self.normalize(img, 8)
+        
+        for x in range(self.N):
+            for y in range(self.N):
+                img[x][y] = int(img[x][y]) >> (8 - self.B)
+
+        return self.normalize(img, 8).astype(np.uint8)
+    
+    def calc_rmse(self, img1, img2):
+        '''
+        Computes RMSE in between two images
+        
+        @param img1: 2D numpy array
+        @param img2: 2D numpy array
+        @return integer with RMSE value
+        '''
+        return round(sqrt(sum(sum(np.multiply((img1 - img2), (img1 - img2))))), 4) 
+        
